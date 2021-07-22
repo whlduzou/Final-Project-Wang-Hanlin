60bb5a6ba18219c8bddecb9f69b29f81bfbb51ce
whlduzou <w_hanlin@qq.com>
2021-07-22 14:35:34 +0800

    Update Bayesian Approach.R

--- Bayesian Approach.R
+++ Bayesian Approach.R
@@ -7,8 +7,6 @@ data <- filter(data, Nation == "England")
 data <- filter(data.time, date > as.Date("2020-01-31"))
 data <- data %>% select(cases, hospital)
 
-## Warning: package 'inline' was built under R version 3.1.3
-
 
 mycode <- "
 data {
@@ -23,6 +21,8 @@ parameters {
   real<lower=0,upper=1> p_servere;
   real<lower=0,upper=1> p_test;
   real<lower=0,upper=1> p_sensitive;
+  real<lower=0,upper=1> test_prior;
+  real<lower=0,upper=1> servere_prior;
   real<lower=0> c[N];
 }
 transformed parameters {
@@ -41,8 +41,10 @@ model {
   c ~ gamma(0.001, 0.001); // Non-informative prior
   p_sampling ~ poission(c);
   p_sympotm ~ beta(39,87);
-  p_servere ~ bernoulli(0.138);
-  p_test ~ bernoulli(0.7448);
+  servere_prior ~ beta(138, 862) // Informative prior
+  p_servere ~ bernoulli(servere_prior);
+  test_prior ~ beta(35,12) // Informative prior
+  p_test ~ bernoulli(test_prior);
   p_sensitive ~ binomial(449, 0.94);
   Hospital ~ binomial(pop, p_hospital);
   Cases ~ binomial(pop, p_cases);
