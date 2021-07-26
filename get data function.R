27b6b07dd89a429cc87c4ea516eb0ddeafaee001
whlduzou <w_hanlin@qq.com>
2021-07-26 23:16:32 +0800

    Update get data function.R

--- get data function.R
+++ get data function.R
@@ -98,278 +98,181 @@ linear.impute <- function(file, j) {
 
 add.policy <- function(data){
   library(dplyr)
-  # data$LEP1 <- 0
-  # data$LEP2 <- 0
-  # data$LRE1 <- 0
-  # data$LRE2 <- 0
-  # data$LRE3 <- 0
-  # data$LRE4 <- 0
-  # data$LRE5 <- 0
-  # data$LRE6 <- 0
-  # data$LRE7 <- 0
-  # data$LRE8 <- 0
-  # data$LRE9 <- 0
-  # data$LRI1 <- 0
-  # data$LRI2 <- 0
-  # data$LRI3 <- 0
-  # data$LRI4 <- 0
-  # data$LRI5 <- 0
-  # data$LRI6 <- 0
-  # data$LRI7 <- 0
-  # data$LRI8 <- 0
-  # data$LRI9 <- 0
-  # data$LRI10 <- 0
-  # data$LRI11 <- 0
-  # data <- arrange(data, date)
-  # # adding the policy data
-  # for (d in 1:nrow(data)) {
-  #   date <- data$date[d]
-  #   if (date >= '2020-03-23') {
-  #     data[d,"LRI1"] <- 1
-  #   }
-  #   if (date >= '2020-03-25') {
-  #     data[d,"LEP1"] <- 1
-  #   }
-  #   if (date >= '2020-03-26') {
-  #     data[d,"LRI2"] <- 1
-  #   }
-  #   if (date >= '2020-04-16') {
-  #     data[d,"LRI3"] <- 1
-  #   }
-  #   if (date >= '2020-05-10') {
-  #     data[d,"LRE1"] <- 1
-  #   }
-  #   if (date >= '2020-06-01') {
-  #     data[d,"LRE2"] <- 1
-  #   }
-  #   if (date >= '2020-06-15') {
-  #     data[d,"LRE3"] <- 1
-  #   }
-  #   if (date >= '2020-06-23') {
-  #     data[d,"LRE4"] <- 1
-  #   }
-  #   if (date >= '2020-07-04') {
-  #     data[d,"LRI4"] <- 1
-  #     data[d,"LRE5"] <- 1
-  #   }
-  #   if (date >= '2020-07-18') {
-  #     data[d,"LEP2"] <- 1
-  #   }
-  #   if (date >= '2020-08-03') {
-  #     data[d,"LRE6"] <- 1
-  #   }
-  #   if (date >= '2020-08-14') {
-  #     data[d,"LRE7"] <- 1
-  #   }
-  #   if (date >= '2020-09-14') {
-  #     data[d,"LRI5"] <- 1
-  #   }
-  #   if (date >= '2020-09-22') {
-  #     data[d,"LRI6"] <- 1
-  #   }
-  #   if (date >= '2020-10-14') {
-  #     data[d,"LRI7"] <- 1
-  #   }
-  #   if (date >= '2020-11-05') {
-  #     data[d,"LRI8"] <- 1
-  #   }
-  #   if (date >= '2020-11-24') {
-  #     data[d,"LRE8"] <- 1
-  #   }
-  #   if (date >= '2020-12-02') {
-  #     data[d,"LRI9"] <- 1
-  #   }
-  #   if (date >= '2020-12-15') {
-  #     data[d,"LRE9"] <- 1
-  #   }
-  #   if (date >= '2020-12-21') {
-  #     data[d,"LRI10"] <- 1
-  #   }
-  #   if (date >= '2020-12-26') {
-  #     data[d,"LRI11"] <- 1
-  #   }
-  # }
-  # pca <- prcomp(data[,(ncol(data)-21):ncol(data)])[["x"]]
-  # data$PCA1 <- pca[,1]
-  # data$PCA2 <- pca[,2]
-  # data$PCA3 <- pca[,3]
-  data$LRI <- 0
-  data$LRE <- 0
+  data$government_announcements <- 0
+  data$Lockdown <- 0
+  data$Legislation <- 0
+  data$schools_universities <- 0
   data <- arrange(data, date)
-  # adding the policy data
-  logit <- function(date_now, date_set, r=0.2) {
-    dif <- as.numeric(difftime(date_now ,date_set))
-    if (dif < 0) {
-      return(0)
-    } else if(dif == 0) {
-      return(1)
-    } else {
-      round(1 - 0.1*exp(r*dif)/(0.9 + 0.1*exp(r*dif)),1)
-    }
-  }
+  # adding the policy data 
   for (d in 1:nrow(data)) {
     date <- data$date[d]
+    if (date >= '2020-03-16') {
+      data[d,"government_announcements"] <- 1
+    }
+    if (date >= '2020-03-19') {
+      data[d,"government_announcements"] <- 0.7
+    }
+    if (date >= '2020-03-21') {
+      data[d,"schools_universities"] <- 1
+    }
     if (date >= '2020-03-23') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-03-23', r=0.05)
+      data[d,"Lockdown"] <- 1
+    }
+    if (date >= '2020-03-25') {
+      data[d,"Legislation"] <- 1
     }
     if (date >= '2020-03-26') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-03-26')
+      data[d,"Lockdown"] <- 1.5
     }
     if (date >= '2020-04-16') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-04-16')
+      data[d,"Lockdown"] <- 2
+    }
+    if (date >= '2020-04-30') {
+      data[d,"government_announcements"] <- 0.6
+    }
+    if (date >= '2020-05-10') {
+      data[d,"government_announcements"] <- 0.3
+      data[d,"Lockdown"] <- 1
     }
     if (date >= '2020-06-01') {
-      data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-06-01')*0.25
+      data[d,"schools_universities"] <- 0.7
     }
     if (date >= '2020-06-15') {
-      data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-06-15')*0.25
+      data[d,"Lockdown"] <- 0.5
+    }
+    if (date >= '2020-05-10') {
+      data[d,"government_announcements"] <- 0
+      data[d,"Lockdown"] <- 0.1
+    }
+    if (date >= '2020-06-29') {
+      data[d,"government_announcements"] <- 0.2
+      data[d,"Lockdown"] <- 0.3
     }
     if (date >= '2020-07-04') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-07-04')*0.25
-      data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-07-04')*0.5
+      data[d,"Lockdown"] <- 0.5
+    }
+    if (date >= '2020-07-18') {
+      data[d,"Legislation"] <- 1.5
     }
     if (date >= '2020-08-03') {
-      data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-08-03')*0.5
+      data[d,"Lockdown"] <- 0.3
     }
     if (date >= '2020-08-14') {
-      data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-08-14')*0.5
+      data[d,"Lockdown"] <- 0
     }
     if (date >= '2020-09-14') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-09-14')*0.25
+      data[d,"Lockdown"] <- 0.3
+    }
+    if (date >= '2020-09-22') {
+      data[d,"Lockdown"] <- 1
+      data[d,"government_announcements"] <- 1
+    }
+    if (date >= '2020-09-30') {
+      data[d,"government_announcements"] <- 1.2
     }
     if (date >= '2020-10-14') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-10-14')*0.5
+      data[d,"Lockdown"] <- 1.2
+    }
+    if (date >= '2020-10-31') {
+      data[d,"Lockdown"] <- 1.4
+      data[d,"government_announcements"] <- 1.4
     }
     if (date >= '2020-11-05') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-11-05')
+      data[d,"Lockdown"] <- 1.5
     }
     if (date >= '2020-11-24') {
-      data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-11-24')
+      data[d,"government_announcements"] <- 0.6
     }
     if (date >= '2020-12-02') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-12-02')
+      data[d,"Lockdown"] <- 2
     }
     if (date >= '2020-12-15') {
-      data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-12-15')
+      data[d,"government_announcements"] <- 0.2
+    }
+    if (date >= '2020-12-19') {
+      data[d,"government_announcements"] <- 0.5
     }
     if (date >= '2020-12-21') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-12-21')
+      data[d,"Lockdown"] <- 2.5
     }
     if (date >= '2020-12-26') {
-      data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-12-26')
+      data[d,"Lockdown"] <- 3
+    }
+    if (date >= '2021-03-07') {
+      data[d,"schools_universities"] <- 0.3
     }
   }
   return(data)
+  # data$LRI <- 0
+  # data$LRE <- 0
+  # data <- arrange(data, date)
+  # # adding the policy data
+  # logit <- function(date_now, date_set, r=0.2) {
+  #   dif <- as.numeric(difftime(date_now ,date_set))
+  #   if (dif < 0) {
+  #     return(0)
+  #   } else if(dif == 0) {
+  #     return(1)
+  #   } else {
+  #     round(1 - 0.1*exp(r*dif)/(0.9 + 0.1*exp(r*dif)),1)
+  #   }
+  # }
+  # for (d in 1:nrow(data)) {
+  #   date <- data$date[d]
+  #   if (date >= '2020-03-23') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-03-23', r=0.05)
+  #   }
+  #   if (date >= '2020-03-26') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-03-26', r=0.05)
+  #   }
+  #   if (date >= '2020-04-16') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-04-16')
+  #   }
+  #   if (date >= '2020-06-01') {
+  #     data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-06-01')*0.25
+  #   }
+  #   if (date >= '2020-06-15') {
+  #     data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-06-15')*0.25
+  #   }
+  #   if (date >= '2020-07-04') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-07-04')*0.25
+  #     data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-07-04')*0.5
+  #   }
+  #   if (date >= '2020-08-03') {
+  #     data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-08-03')*0.5
+  #   }
+  #   if (date >= '2020-08-14') {
+  #     data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-08-14')*0.5
+  #   }
+  #   if (date >= '2020-09-14') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-09-14')*0.25
+  #   }
+  #   if (date >= '2020-10-14') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-10-14')*0.5
+  #   }
+  #   if (date >= '2020-11-05') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-11-05')
+  #   }
+  #   if (date >= '2020-11-24') {
+  #     data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-11-24')
+  #   }
+  #   if (date >= '2020-12-02') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-12-02')
+  #   }
+  #   if (date >= '2020-12-15') {
+  #     data[d,"LRE"] <- data[d,"LRE"] + logit(date, '2020-12-15')
+  #   }
+  #   if (date >= '2020-12-21') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-12-21')
+  #   }
+  #   if (date >= '2020-12-26') {
+  #     data[d,"LRI"] <- data[d,"LRI"] + logit(date, '2020-12-26')
+  #   }
+  #}
+  
 }
 
-# add.policy <- function(data){
-#   library(dplyr)
-#   data$Government_announcements <- 0
-#   data$Lockdown <- 0
-#   data$Legislation <- 0
-#   data$schools_universities <- 0
-#   data <- arrange(data, date)
-#   # adding the policy data
-#   for (d in 1:nrow(data)) {
-#     date <- data$date[d]
-#     if (date >= '2020-03-16') {
-#       data[d,"government_announcements"] <- 1
-#     }
-#     if (date >= '2020-03-19') {
-#       data[d,"government_announcements"] <- 0.7
-#     }
-#     if (date >= '2020-03-21') {
-#       data[d,"schools_universities"] <- 1
-#     }
-#     if (date >= '2020-03-23') {
-#       data[d,"Lockdown"] <- 1
-#     }
-#     if (date >= '2020-03-25') {
-#       data[d,"Legislation"] <- 1
-#     }
-#     if (date >= '2020-03-26') {
-#       data[d,"Lockdown"] <- 1.5
-#     }
-#     if (date >= '2020-04-16') {
-#       data[d,"Lockdown"] <- 2
-#     }
-#     if (date >= '2020-04-30') {
-#       data[d,"government_announcements"] <- 0.6
-#     }
-#     if (date >= '2020-05-10') {
-#       data[d,"government_announcements"] <- 0.3
-#       data[d,"Lockdown"] <- 1
-#     }
-#     if (date >= '2020-06-01') {
-#       data[d,"schools_universities"] <- 0.7
-#     }
-#     if (date >= '2020-06-15') {
-#       data[d,"Lockdown"] <- 0.5
-#     }
-#     if (date >= '2020-05-10') {
-#       data[d,"government_announcements"] <- 0
-#       data[d,"Lockdown"] <- 0.1
-#     }
-#     if (date >= '2020-06-29') {
-#       data[d,"government_announcements"] <- 0.2
-#       data[d,"Lockdown"] <- 0.3
-#     }
-#     if (date >= '2020-07-04') {
-#       data[d,"Lockdown"] <- 0.5
-#     }
-#     if (date >= '2020-07-18') {
-#       data[d,"Legislation"] <- 1.5
-#     }
-#     if (date >= '2020-08-03') {
-#       data[d,"Lockdown"] <- 0.3
-#     }
-#     if (date >= '2020-08-14') {
-#       data[d,"Lockdown"] <- 0
-#     }
-#     if (date >= '2020-09-14') {
-#       data[d,"Lockdown"] <- 0.3
-#     }
-#     if (date >= '2020-09-22') {
-#       data[d,"Lockdown"] <- 1
-#       data[d,"government_announcements"] <- 1
-#     }
-#     if (date >= '2020-09-30') {
-#       data[d,"government_announcements"] <- 1.2
-#     }
-#     if (date >= '2020-10-14') {
-#       data[d,"Lockdown"] <- 1.2
-#     }
-#     if (date >= '2020-10-31') {
-#       data[d,"Lockdown"] <- 1.4
-#       data[d,"government_announcements"] <- 1.4
-#     }
-#     if (date >= '2020-11-05') {
-#       data[d,"Lockdown"] <- 1.5
-#     }
-#     if (date >= '2020-11-24') {
-#       data[d,"government_announcements"] <- 0.6
-#     }
-#     if (date >= '2020-12-02') {
-#       data[d,"Lockdown"] <- 2
-#     }
-#     if (date >= '2020-12-15') {
-#       data[d,"government_announcements"] <- 0.2
-#     }
-#     if (date >= '2020-12-19') {
-#       data[d,"government_announcements"] <- 0.5
-#     }
-#     if (date >= '2020-12-21') {
-#       data[d,"Lockdown"] <- 2.5
-#     }
-#     if (date >= '2020-12-26') {
-#       data[d,"Lockdown"] <- 3
-#     }
-#     if (date >= '2021-03-07') {
-#       data[d,"schools_universities"] <- 0.3
-#     }
-#   }
-#   return(data)
-# }
+
 
 
 get.env.data <- function(AREA_NAME) {
